a99a901e0ff6af332555f049e2ac653178097473
Highbg00 <68878698+Highbg00@users.noreply.github.com>
2022-08-23 15:49:13 +0900

    엔티티 클래스 추가

---
+++ scheduler/src/main/java/org/zerock/scheduler/entity/Schduler.java
@@ -0,0 +1,16 @@
+package org.zerock.scheduler.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Schduler {
+    
+    //데이터 테이블에 들어갈 목록들을 넣는 곳
+}
