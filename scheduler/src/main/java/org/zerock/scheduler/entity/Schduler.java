969cf0b072dcf7b161b07580d5ff73cc8c2e8019
Highbg00 <68878698+Highbg00@users.noreply.github.com>
2022-08-31 15:57:12 +0900

    커밋용

--- scheduler/src/main/java/org/zerock/scheduler/entity/Schduler.java
+++ scheduler/src/main/java/org/zerock/scheduler/entity/Schduler.java
@@ -16,9 +16,11 @@ public class Schduler extends BaseEntity{
     
     //데이터 테이블에 들어갈 데이터 칼럼들을 생성하는 곳
     @Id
-    private String id;
+    private String email;
 
     @Column(length = 100)
     private String pw;
 
+    @Column(length = 10)
+    private String gender;
 }
--- scheduler/src/main/java/org/zerock/scheduler/repository/SchedulerRepository.java
+++ scheduler/src/main/java/org/zerock/scheduler/repository/SchedulerRepository.java
@@ -1,7 +1,8 @@
 package org.zerock.scheduler.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.zerock.scheduler.entity.Schduler;
 
-public interface SchedulerRepository extends JpaRepository<Schduler, String> {
+public interface SchedulerRepository extends JpaRepository<Schduler, String>, QuerydslPredicateExecutor<Schduler> {
 }
